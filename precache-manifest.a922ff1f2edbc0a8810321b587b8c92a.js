5d34873cae1073f8cdbd0ce474dcbe2fc8e0cbe1
Abhishek Wahane <abhishekpwahane@gmail.com>
2020-08-05 15:03:48 +0530

    Updates

---
+++ precache-manifest.a922ff1f2edbc0a8810321b587b8c92a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dede37498edd137e8b04866730ac057",
+    "url": "/covid_tracker/index.html"
+  },
+  {
+    "revision": "701d20023a085f977f02",
+    "url": "/covid_tracker/static/css/main.02d14cc4.chunk.css"
+  },
+  {
+    "revision": "7ebcce8c030fa1540a78",
+    "url": "/covid_tracker/static/js/2.16b9e052.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid_tracker/static/js/2.16b9e052.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "701d20023a085f977f02",
+    "url": "/covid_tracker/static/js/main.4095dbdb.chunk.js"
+  },
+  {
+    "revision": "4c5145b2abee34e7044b",
+    "url": "/covid_tracker/static/js/runtime-main.fb184295.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid_tracker/static/media/image.d7265326.png"
+  }
+]);
